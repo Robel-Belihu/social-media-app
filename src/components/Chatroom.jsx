e2faade62678865c8bf28c0b3755bcb7bb777b1c
Robel-Belihu <robelmelalku334@gmail.com>
2023-05-07 15:39:44 +0300

    styled the chatroom

--- src/components/Chatroom.jsx
+++ src/components/Chatroom.jsx
@@ -8,6 +8,7 @@ import {
   where,
 } from "firebase/firestore";
 import { auth, db } from "../firebase.config";
+import "../index.css";
 
 export function Chatroom({ room }) {
   const [newMessage, setNewMessage] = useState("");
@@ -41,38 +42,129 @@ export function Chatroom({ room }) {
   };
 
   return (
-    <div class="bg-blue-200">
-      <div className="md:items-center md:justify-center md:border-2xl">
-        <form
-          onSubmit={handleSubmit}
-          className="container h-screen flex justify-center items-center"
-        >
-          <div className="flex">
-            <h2>{Chatroom}</h2>
-            <div className="border-2">
-              {messages.map((message) => (
-                <h2>{message.name}</h2>
-              ))}
-              <div className="flex w-full items-center">
-                <input
-                  type="text"
-                  className="h-14 w-96 pl-10 pr-20 mt-4 rounded-l-lg z-0 focus:shadow focus:outline-none"
-                  placeholder="Type a text ..."
-                  onChange={(e) => setNewMessage(e.target.value)}
-                  value={newMessage}
-                />
-
-                <button
-                  type="submit"
-                  className="text-white text-xl h-14 pl-10 pr-20 mt-4 rounded-r-lg z-0 bg-orange-600 hover:bg-orange-400"
-                >
-                  Send
-                </button>
+    <div className="h-screen bg-hero bg-cover bg-no-repeat">
+      <form
+        onSubmit={handleSubmit}
+        className="flex-1 p:2 sm:p-6 justify-between flex flex-col h-screen"
+      >
+        <div className="flex sm:items-center justify-center py-3 border-b-2 border-gray-200">
+          <div className="relative flex items-center space-x-4">
+            <div className="relative">
+              <span className="absolute text-green-500 right-0 bottom-0">
+                <svg width="20" height="20">
+                  <circle cx="8" cy="8" r="8" fill="currentColor"></circle>
+                </svg>
+              </span>
+              <img
+                src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144"
+                alt=""
+                className="w-10 sm:w-16 h-10 sm:h-16 rounded-full"
+              />
+            </div>
+            <div className="flex flex-col leading-tight">
+              <div className="text-2xl mt-1 flex items-center">
+                <span className="text-gray-700 mr-3">
+                  Welcome to chatroom{" "}
+                  <span className="font-black text-purple-800">{room}</span>
+                </span>
               </div>
             </div>
           </div>
-        </form>
-      </div>
+          <div className="flex items-center space-x-2"></div>
+        </div>
+        <div
+          id="messages"
+          className="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+        >
+          <div className="chat-message">
+            <div className="flex items-end">
+              <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
+                {messages.map((message) => (
+                  <span className="px-4 py-2 rounded-lg text-sm inline-block rounded-bl-none bg-gray-300 text-black">
+                    <span className="font-semibold text-base text-purple-600">
+                      {message.user}
+                    </span>
+                    : {message.name}
+                  </span>
+                ))}
+              </div>
+              <img
+                src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144"
+                alt="My profile"
+                className="w-6 h-6 rounded-full order-1"
+              />
+            </div>
+          </div>
+          <div className="chat-message">
+            <div className="flex items-end justify-end">
+              <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
+                <div>
+                  <span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">
+                    Your error message says permission denied, npm global
+                    installs must be given root privileges.
+                  </span>
+                </div>
+              </div>
+              <img
+                src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144"
+                alt="My profile"
+                className="w-6 h-6 rounded-full order-2"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
+          <div className="relative flex">
+            <input
+              type="text"
+              placeholder="Type a text ..."
+              onChange={(e) => setNewMessage(e.target.value)}
+              value={newMessage}
+              className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3"
+            />
+            <div class="absolute right-0 items-center inset-y-0 hidden sm:flex">
+              <button
+                type="button"
+                className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="h-6 w-6 text-gray-600"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  ></path>
+                </svg>
+              </button>
+              <button
+                type="button"
+                className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
+              >
+                <span class="font-bold">Send</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-6 w-6 ml-2 transform rotate-90"
+                >
+                  <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+
+      <script>
+        const el = document.getElementById('messages') el.scrollTop =
+        el.scrollHeight
+      </script>
     </div>
   );
 }

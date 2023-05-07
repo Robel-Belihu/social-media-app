01753417fb09d5e1d4c83118fd6d8a93d1f44942
Robel-Belihu <robelmelalku334@gmail.com>
2023-05-07 10:11:22 +0300

    applied useEffect and cleanup

--- src/components/Chatroom.jsx
+++ src/components/Chatroom.jsx
@@ -1,11 +1,30 @@
-import React, { useState } from "react";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import React, { useState, useEffect } from "react";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  serverTimestamp,
+  where,
+} from "firebase/firestore";
 import { auth, db } from "../firebase.config";
 
 export function Chatroom({ room }) {
   const [newMessage, setNewMessage] = useState("");
-
+  const [messages, setMessages] = useState([]);
   const messagesRef = collection(db, "messages");
+
+  useEffect(() => {
+    const queryMsg = query(messagesRef, where("room", "==", room));
+    const unsubscribe = onSnapshot(queryMsg, (snapshot) => {
+      let texts = [];
+      snapshot.forEach((snap) => {
+        texts.push({ ...snap.data(), id: snap.id });
+      });
+      setMessages(texts);
+    });
+    return () => unsubscribe();
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,32 +41,38 @@ export function Chatroom({ room }) {
   };
 
   return (
-    <div class=" bg-blue-200">
-      <form
-        onSubmit={handleSubmit}
-        className="container h-screen flex justify-center items-start"
-      >
-        <div className="relative">
-          <div className="absolute top-4 left-3">
-            <i className="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
+    <div class="bg-blue-200">
+      <div className="md:items-center md:justify-center md:border-2xl">
+        <form
+          onSubmit={handleSubmit}
+          className="container h-screen flex justify-center items-center"
+        >
+          <div className="flex">
+            <h2>{Chatroom}</h2>
+            <div className="border-2">
+              {messages.map((message) => (
+                <h2>{message.name}</h2>
+              ))}
+              <div className="flex w-full items-center">
+                <input
+                  type="text"
+                  className="h-14 w-96 pl-10 pr-20 mt-4 rounded-l-lg z-0 focus:shadow focus:outline-none"
+                  placeholder="Type a text ..."
+                  onChange={(e) => setNewMessage(e.target.value)}
+                  value={newMessage}
+                />
+
+                <button
+                  type="submit"
+                  className="text-white text-xl h-14 pl-10 pr-20 mt-4 rounded-r-lg z-0 bg-orange-600 hover:bg-orange-400"
+                >
+                  Send
+                </button>
+              </div>
+            </div>
           </div>
-          <input
-            type="text"
-            className="h-14 w-96 pl-10 pr-20 mt-4 rounded-lg z-0 focus:shadow focus:outline-none"
-            placeholder="Type a text ..."
-            onChange={(e) => setNewMessage(e.target.value)}
-            value={newMessage}
-          />
-          <div className="absolute top-2 right-2">
-            <button
-              type="submit"
-              className="h-10 w-20 text-white rounded-lg bg-red-500 hover:bg-red-600"
-            >
-              Search
-            </button>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 }
